e3d9abaa492959954bbd8fced518d0ccf06d5030
xbSnake <lementarev_anton@mail.ru>
2017-03-06 21:38:18 +0200

    ыы

--- 1.JavaSyntax/src/com/javarush/task/task04/task0426/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0426/Solution.java
@@ -8,6 +8,6 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        //напишите тут вавсіваш код
     }
 }
