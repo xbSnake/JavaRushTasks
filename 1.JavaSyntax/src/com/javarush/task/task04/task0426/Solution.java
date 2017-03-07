4f0eb1b01d7afa7a75cc6912fb5436c7d2f57ee0
xbSnake <lementarev_anton@mail.ru>
2017-03-07 07:21:40 +0200

    ыы

--- 1.JavaSyntax/src/com/javarush/task/task04/task0426/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0426/Solution.java
@@ -1,13 +1,26 @@
 package com.javarush.task.task04.task0426;
 
-/* 
-Ярлыки и числа
+
+/*
+Цель установлена!
 */
 
 import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут вавсіваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+
+        if (a<0 && a%2==0 )
+            System.out.println("отрицательное четное число");
+        if(a<0 && a%2!=0)
+            System.out.println("отрицательное нечетное число");
+        if(a==0)
+            System.out.println("ноль");
+        if(a>0 && a%2==0)
+            System.out.println("положительное четное число");
+        if (a>0 && a%2!=0)
+            System.out.println("положительное нечетное число");
     }
 }
