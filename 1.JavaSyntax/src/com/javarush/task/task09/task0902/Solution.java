b094bec64473319bee8a85b95de6bab2b86216d4
xbSnake <lementarev_anton@mail.ru>
2017-03-13 20:05:02 +0200

    ыы

--- 1.JavaSyntax/src/com/javarush/task/task09/task0902/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0902/Solution.java
@@ -12,29 +12,29 @@ public class Solution {
     public static String method1() {
         method2();
         StackTraceElement[] stackTraceElement =Thread.currentThread().getStackTrace();
-        return stackTraceElement[0].getMethodName();
+        return stackTraceElement[2].getMethodName();
     }
 
     public static String method2() {
         method3();
         StackTraceElement[] stackTraceElement =Thread.currentThread().getStackTrace();
-        return stackTraceElement[0].getMethodName();
+        return stackTraceElement[2].getMethodName();
     }
 
     public static String method3() {
         method4();
         StackTraceElement[] stackTraceElement =Thread.currentThread().getStackTrace();
-        return stackTraceElement[0].getMethodName();
+        return stackTraceElement[2].getMethodName();
     }
 
     public static String method4() {
         method5();
         StackTraceElement[] stackTraceElement =Thread.currentThread().getStackTrace();
-        return stackTraceElement[0].getMethodName();
+        return stackTraceElement[2].getMethodName();
     }
 
     public static String method5() {
         StackTraceElement[] stackTraceElement =Thread.currentThread().getStackTrace();
-        return stackTraceElement[0].getMethodName();
+        return stackTraceElement[2].getMethodName();
     }
 }
